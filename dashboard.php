5b4293a2489b4b1058b98b8551cce671b5c9b582
okputadora <okputadora@gmail.com>
2016-12-19 20:47:23 -0500

    Dashboard page

---
+++ dashboard.php
@@ -0,0 +1,55 @@
+<?php include_once("navbartemplate.php") ?>
+<link rel="stylesheet" type = "text/css"
+ href = "dashboardstyle.css">
+<div class="main-content" >
+  <div class="col-4">
+    <div id="tab-container">
+      <div id="schedule-tab" style=>Schedule</div>
+      <div id="harvest-tab">Harvest Logs</div>
+      <div id="seeding-tab">Seeding</div>
+      <div id="planting-tab">Planting</div>
+      <div id="field-tab">Field Map</div>
+    </div>
+  </div>
+  <div class="col-2" id="nav">
+    <div id="tasks">
+      <div class="title">Tasks</div>
+      <div id="week-cal">
+        <div class="day" id="d1">
+          <div class="dayname" id="name1">Day 1</div>
+          <div class="daycontent">Content</div>
+        </div>
+        <div class="day" id="d2">
+          <div class="dayname" id="name2">Day 2</div>
+          <div class="daycontent">Content</div>
+        </div>
+        <div class="day" id="d3">
+          <div class="dayname" id="name3">Day 3</div>
+          <div class="daycontent">Content</div>
+        </div>
+        <div class="day" id="d4">
+          <div class="dayname" id="name4">Day 4</div>
+          <div class="daycontent">Content</div>
+        </div>
+        <div class="day" id="d5">
+          <div class="dayname" id="name5">Day 5</div>
+          <div class="daycontent">Content</div>
+        </div>
+        <div class="day" id="d6">
+          <div class="dayname" id="name6">Day 6</div>
+          <div class="daycontent">Content</div>
+        </div>
+        <div class="day" id="d7">
+          <div class="dayname" id="name7">Day 7</div>
+          <div class="daycontent">Content</div>
+        </div>
+      </div>
+      <div id="day-description">
+
+      </div>
+    </div>
+  </div>
+  <div class="col-4">
+
+  </div>
+</div>
