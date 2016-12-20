af5ed2952554509b6546f134f9d231b3a165cddd
okputadora <okputadora@gmail.com>
2016-12-19 20:46:35 -0500

    Simple navbar layout for site homepage

---
+++ navbartemplate.php
@@ -0,0 +1,22 @@
+<head>
+  <link rel="stylesheet" type = "text/css"
+   href = "navbarstyle.css">
+</head>
+<body>
+  <div class="navBar">
+    <div class="banner">
+      <h3>%page%</h3>
+      <div class="search">
+        Search
+      </div>
+    </div>
+    <div class="menu">
+      <div>Sign In</div>
+      <div>Sign Up</div>
+      <div>%logo%</div>
+      <div>About Us</div>
+      <div>Demo</div>
+    </div>
+  </div>
+
+</body>
