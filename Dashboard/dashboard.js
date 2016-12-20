c2e61cd2776021a42e65a33e41becfaf395f2ff6
okputadora <okputadora@gmail.com>
2016-12-20 13:42:42 -0500

    Restructure files

---
+++ Dashboard/dashboard.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+// console.log("working");
+// if (navigator.geolocation){
+//   navigator.geolocation.getCurrentPosition(function(position){
+//
+//     lat = position.coords.latitude;
+//     lat = lat.toFixed(3);
+//     lon = position.coords.longitude;
+//     lon = lon.toFixed(3);
+//     $("#weather-map").html("Latitude: " + lat +
+//   "</br>Longitude: " + lon);
+//   var url = "http://api.wunderground.com/api/d8483e016960a875/animatedradar/animatedsattelite/image.gif?num=8&delay=25&rad.maxlat=" +
+//   (lat+5) + "&rad.maxlon=" + (lon+5)+ "&rad.minlat=" + (lat-5) +
+//   "&rad.minlon="+ (lon-5) + "&rad.width=640&rad.height=480&rad.rainsnow=1&rad.reproj.automerc=1&rad.num=5&" +
+//   "sat.maxlat=" + (lat+5) + "&sat.maxlon=" + (lon +5) +
+//   "&sat.minlat=" + (lat-5) + "&sat.minlon=" + (lon -5) + "&sat.width=640&sat.height=480&sat.key=sat_ir4_bottom&sat.gtt=107&sat.proj=me&sat.timelabel=0&sat.num=5callback=?";
+// url = "http://api.wunderground.com/api/d8483e016960a875/animatedradar/animatedsatellite/image.gif?num=8&delay=25&rad.maxlat=47.709&rad.maxlon=-69.263&rad.minlat=31.596&rad.minlon=-97.388&rad.width=640&rad.height=480&rad.rainsnow=1&rad.reproj.automerc=1&rad.num=5&sat.maxlat=47.709&sat.maxlon=-69.263&sat.minlat=31.596&sat.minlon=-97.388&sat.width=640&sat.height=480&sat.key=sat_ir4_bottom&sat.gtt=107&sat.proj=me&sat.timelabel=0&sat.num=5callback=?"
+//   $.getJSON(url, function(data){
+//     console.log(data);
+//   }, "jsonp");
+// $("#weather-map").html(url);
+//   console.log(url);
+//   })
+// }
+if (navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(function(position){
+    lat = position.coords.latitude;
+    lat = lat.toFixed(3);
+    lon = position.coords.longitude;
+    lon = lon.toFixed(3);
+    $("#weather-map").html("Latitude: " + lat +
+     "</br>Longitude: " + lon);
+     url = "http://api.wunderground.com/api/d8483e016960a875/geolookup/q/" + lat +
+     "," + lon + ".json";
+     $.getJSON(url, function(data){
+       console.log(url);
+       console.log(data);
+       var city = data.location.city;
+       var state = data.location.state;
+       url = "http://api.wunderground.com/api/d8483e016960a875/forecast10day/q/" + state + "/" + city+".json"
+       $.getJSON(url, function(data){
+         console.log(url);
+         console.log(data);
+         var day1 = data.forecast.txt_forecast.forecastday[0].title;
+         var day2 = data.forecast.txt_forecast.forecastday[2].title;
+         var day3 = data.forecast.txt_forecast.forecastday[4].title;
+         var i1 = data.forecast.txt_forecast.forecastday[0].icon_url;
+         var i2 = data.forecast.txt_forecast.forecastday[2].icon_url;
+         var i3 = data.forecast.txt_forecast.forecastday[4].icon_url;
+         var des1 = data.forecast.txt_forecast.forecastday[0].fcttext;
+         var des2 = data.forecast.txt_forecast.forecastday[2].fcttext;
+         var des3 = data.forecast.txt_forecast.forecastday[4].fcttext;
+         var temph1 = data.forecast.simpleforecast.forecastday[0].high.fahrenheit;
+         var temph2 = data.forecast.simpleforecast.forecastday[2].high.fahrenheit;
+         var temph3 = data.forecast.simpleforecast.forecastday[4].high.fahrenheit;
+         var templ1 = data.forecast.simpleforecast.forecastday[0].low.fahrenheit;
+         var templ2 = data.forecast.simpleforecast.forecastday[2].low.fahrenheit;
+         var templ3 = data.forecast.simpleforecast.forecastday[4].low.fahrenheit;
+         $("#ft1").html(day1);
+         $("#ft2").html(day2);
+         $("#ft3").html(day3);
+         $("#fi1").html("<img src='"+i1+"'/>");
+         $("#fi2").html("<img src='"+i2+"'/>");
+         $("#fi3").html("<img src='"+i3+"'/>");
+         $("#fth1").html("H:" + temph1 + "℉");
+         $("#ftl1").html("L:" + templ1 + "℉");
+         $("#fth2").html("H:" + temph2 + "℉");
+         $("#ftl2").html("L:" + templ2 + "℉");
+         $("#fth3").html("H:" + temph3 + "℉");
+         $("#ftl3").html("L:" + templ3 + "℉");
+         $("#wd1").html(des1);
+         $("#wd2").html(des2);
+         $("#wd3").html(des3);
+       }, "jsonp")
+     }, "jsonp")
+   })
+ }
+ $("#fday1").hover(function(){
+   $("#wd1").css("display", "block");
+   $("#wd2").css("display", "none");
+   $("#wd3").css("display", "none");
+ });
+ $("#fday2").hover(function(){
+   $("#wd2").css("display", "block");
+   $("#wd1").css("display", "none");
+   $("#wd3").css("display", "none");
+ });
+ $("#fday3").hover(function(){
+   $("#wd3").css("display", "block");
+   $("#wd1").css("display", "none");
+   $("#wd2").css("display", "none");
+ });
+})
---
+++ Dashboard/dashboard.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="en" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="content-type" content="text/xml; charset=utf-8" />
+  <!--ensures proper rendering and touch zooming on mobile devices-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Local Weather</title></title>
+  <!--import google font-->
+<link href="https://fonts.googleapis.com/css?family=Yantramanav" rel="stylesheet">
+  <!--import style sheet-->
+  <link rel="stylesheet" type = "text/css"
+   href = "dashboardstyle.css">
+</head>
+
+
+
+<body>
+<?php include_once("navbartemplate.php") ?>
+<div class="main-content" >
+  <div class="col-4">
+    <div id="tab-container">
+      <div id="schedule-tab"  >Schedule</div>
+      <div id="harvest-tab">Harvest Logs</div>
+      <div id="seeding-tab">Seeding</div>
+      <div id="planting-tab">Planting</div>
+      <div id="field-tab">Field Map</div>
+      <div id="analysis-tab">Analysis</div>
+    </div>
+  </div>
+  <div class="col-2" id="nav">
+    <div class="container" id="tasks">
+      <div class="c-banner">
+        <div class="title">Tasks</div>
+      </div>
+      <div id="week-cal">
+        <div class="day" id="d1">
+          <div class="dayname" id="name1">Day 1</div>
+          <div class="daycontent">Content</div>
+        </div>
+        <div class="day" id="d2">
+          <div class="dayname" id="name2">Day 2</div>
+          <div class="daycontent">Content</div>
+        </div>
+        <div class="day" id="d3">
+          <div class="dayname" id="name3">Day 3</div>
+          <div class="daycontent">Content</div>
+        </div>
+        <div class="day" id="d4">
+          <div class="dayname" id="name4">Day 4</div>
+          <div class="daycontent">Content</div>
+        </div>
+        <div class="day" id="d5">
+          <div class="dayname" id="name5">Day 5</div>
+          <div class="daycontent">Content</div>
+        </div>
+        <div class="day" id="d6">
+          <div class="dayname" id="name6">Day 6</div>
+          <div class="daycontent">Content</div>
+        </div>
+        <div class="day" id="d7">
+          <div class="dayname" id="name7">Day 7</div>
+          <div class="daycontent">Content</div>
+        </div>
+      </div>
+      <div id="day-description">
+
+      </div>
+    </div>
+    <div class="container" id="weather">
+      <div class="c-banner">
+        <div class="title">Weather</div>
+      </div>
+      <div id="forecast">
+        <div class="fday" id="fday1">
+          <div class="ftitle" id="ft1"></div>
+          <div class="ficon" id="fi1"></div>
+          <div class="ftemp">
+            <div class="ftemph" id="fth1"></div>
+            <div class="ftempl" id="ftl1"></div>
+          </div>
+        </div>
+        <div class="fday" id="fday2">
+          <div class="ftitle" id="ft2"></div>
+          <div class="ficon" id="fi2"></div>
+          <div class="ftemp">
+            <div class="ftemph" id="fth2"></div>
+            <div class="ftempl" id=ftl2></div>
+          </div>
+        </div>
+        <div class="fday" id="fday3">
+          <div class="ftitle" id="ft3"></div>
+          <div class="ficon" id="fi3"></div>
+          <div class="ftemp">
+            <div class="ftemph" id="fth3"></div>
+            <div class="ftempl" id="ftl3"></div>
+          </div>
+        </div>
+      </div>
+      <div id="weather-description">
+        <p id="wd1"></p>
+        <p id="wd2"></p>
+        <p id="wd3"></p>
+      </div>
+      <div id="weather-opts">
+        <div class="wopt">View Radar</div>
+        <div class="wopt">Hourly Forecast</div>
+        <div class="wopt">10 Day Forecast</div>
+        <div class="wopt">Soil Temperature</div>
+        <div class="wopt">Hisotrical Data</div>
+      </div>
+    </div>/
+    <div class = "container" id="week-harvest">
+      <div class="c-banner">
+        <div class="title">Next Harvest</div>
+    </div>
+  </div>
+  <div class="col-4">
+
+  </div>
+</div>
+</body>
+<script src="jQuery.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src ="dashboard.js"></script>
