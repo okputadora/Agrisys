ccb6a33a4f7a31126b89b34934079fa3a329a670
okputadora <okputadora@gmail.com>
2016-12-20 14:58:10 -0500

    Basic homepage layout

--- Homepage/homepage.php
+++ Homepage/homepage.php
@@ -1,3 +1,85 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="en" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="content-type" content="text/xml; charset=utf-8" />
+  <!--ensures proper rendering and touch zooming on mobile devices-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Agrisyst</title></title>
+  <!--import google font-->
+<link href="https://fonts.googleapis.com/css?family=Yantramanav" rel="stylesheet">
+  <!--import style sheet-->
+  <link rel="stylesheet" type = "text/css"
+   href = "homepagestyle.css">
+</head>
+
+<body>
 <?php
 include_once("../navbartemplate.php");
 ?>
+<div id="main-content">
+  <div class="sub-container"id="intro">
+    <h1>AgriSyst is a free, opensource farm management software package
+      designed to help you plan your season</h1>
+    <div id="key-features">
+      <div class="k-feature" id="seed-select">
+        <div class="feature-img"><img src="seed.jpg" /></div>
+        <div class="feature-des">From Selecting Seeds...</div>
+      </div>
+      <div class="k-feature" id="seed-select">
+        <div class="feature-img"><img src="harvest.jpg" /></div>
+        <div class="feature-des">...To Planning Harvests...</div>
+      </div>
+      <div class="k-feature" id="seed-select">
+        <div class="feature-img"><img src="sales.jpg" /></div>
+        <div class="feature-des">...To tracking Sales</div>
+      </div>
+    </div>
+    <h3>AgriSyst makes it easy to run an efficient farm of any size</h3>
+  </div>
+  <div class="sub-container"id="features">
+    <h3>Key Features Include...</h3>
+    <div id="feature-list">
+      <div class="feature">
+        <div class="box"></div>
+        <div class="f-title">Custom Setup based on the type of operation you run</div>
+      </div>
+      <div class="feature">
+        <div class="box"></div>
+        <div class="f-title">Built in seed catalogs with up to date pricing</div>
+      </div>
+      <div class="feature">
+        <div class="box"></div>
+        <div class="f-title">Integrated field map</div>
+      </div>
+      <div class="feature">
+        <div class="box"></div>
+        <div class="f-title">Live weather updates and alerts integrated into your schedule</div>
+      </div>
+      <div class="feature">
+        <div class="box"></div>
+        <div class="f-title">Project tracking and management</div>
+      </div>
+      <div class="feature">
+        <div class="box"></div>
+        <div class="f-title">Auto-generated harvest logs</div>
+      </div>
+      <div class="feature">
+        <div class="box"></div>
+        <div class="f-title">Compare actual and predicted data</div>
+      </div>
+      <div class="feature">
+        <div class="box"></div>
+        <div class="f-title">Export data to Excel or Google Docs</div>
+      </div>
+      <div class="feature">
+        <div class="box"></div>
+        <div class="f-title">Auto-generated planting and seeding schedules based on when you want to harvest</div>
+      </div>
+      <div class="feature">
+        <div class="box"></div>
+        <div class="f-title"></div>
+      </div>
+    </div>
+  </div>
+</div>
